72f7be73d9c0614cdeb751111eb60e5baa27104b
Misha Kalita <mishakalita3@gmail.com>
2021-10-09 21:03:56 +0300

    docker

--- dom24/urls.py
+++ dom24/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('', include(('main.urls', 'main'), namespace='main')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
